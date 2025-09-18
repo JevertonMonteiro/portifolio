9d2d1ab1fee0be456b3819daef1a0472f671c28b
JevertonMonteiro <147225632+JevertonMonteiro@users.noreply.github.com>
2025-09-18 13:08:03 -0300

    Update script.js

--- rsc/js/script.js
+++ rsc/js/script.js
@@ -1,11 +1,18 @@
-function enviarWhatsApp(event) {
-    event.preventDefault();
-     // Envia as infomações para o Whatsapp
-    const nome = document.getElementById("nome").value;
-    const mensagem = document.getElementById("mensagem").value;
-    const numero = "5582982201611";
+document.addEventListener('DOMContentLoaded', function() {
+    const menuIcon = document.querySelector('.menu-icon');
+    const menuContainer = document.querySelector('.menu-container');
 
-    const url = `https://api.whatsapp.com/send?phone=${numero}&text=Olá,%20meu%20nome%20é%20${encodeURIComponent(nome)}.%20${encodeURIComponent(mensagem)}`;
+    menuIcon.addEventListener('click', function() {
+        menuContainer.classList.toggle('active');
+        menuIcon.classList.toggle('active');
+    });
 
-    window.open(url, "_blank");
-}
+    // Fechar o menu ao clicar em um link
+    const menuLinks = document.querySelectorAll('.menu-link');
+    menuLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            menuContainer.classList.remove('active');
+            menuIcon.classList.remove('active');
+        });
+    });
+    });
